0bdd1c7afae1c7e1a5657cfc14ef398810482e8c
“phucdev” <vanphuc0295@gmail.com>
2023-10-17 20:57:51 +0700

    add table and getUser

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import Table from "react-bootstrap/Table";
+import { fetUsers } from "../services/userServive";
 
-const TableUsers = () => {
+function TableUsers() {
+  const [listUsers, setlistUsers] = useState([]);
+  useEffect(() => {
+    const getUsers = async () => {
+      const res = await fetUsers();
+
+      setlistUsers(res.data.data);
+    };
+    getUsers();
+  }, []);
+  console.log(listUsers);
   return (
-    <div>TableUsers</div>
-  )
+    <Table striped bordered hover variant="light">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Email</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        {listUsers &&
+          listUsers.map((item) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.email}</td>
+              <td>{item.first_name}</td>
+              <td>{item.last_name}</td>
+            </tr>
+          ))}
+      </tbody>
+    </Table>
+  );
 }
 
-export default TableUsers
+export default TableUsers;
