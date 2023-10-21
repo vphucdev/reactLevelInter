f213ae1073fe2568a17013bf1dbd5ed1d66d93ff
“phucdev” <vanphuc0295@gmail.com>
2023-10-21 16:47:42 +0700

    add btn AddNewUser

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import { Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import "./App.scss";
 import Header from "./components/Header";
 import TableUsers from "./components/TableUsers";
+import ModalAddNew from "./components/ModalAddNew";
+import { useState } from "react";
 
 function App() {
+  const [isShowModalAddNew, setisShowModalAddNew] = useState(false)
   return (
     <div className="App">
       <Header />
-      
-      <Container> 
+
+      <Container>
+        <div className="my-3 d-flex justify-content-between align-items-center">
+          <span>List Users: </span>
+          <Button variant="success" onClick={()=>setisShowModalAddNew(true)}>Add New User</Button>
+        </div>
         <TableUsers />
       </Container>
+      <ModalAddNew show={isShowModalAddNew} handleClose={()=>setisShowModalAddNew(false)} />
     </div>
     // bọc Container ben ngoai để TableUsers không bị dính sát vào 2 bên lề
   );
