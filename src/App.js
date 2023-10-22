f423f1eb81671c12da70c670ada55e1fef8cda9f
“phucdev” <vanphuc0295@gmail.com>
2023-10-22 16:36:32 +0700

    use toastify, move <ModalAddNew/> into TableUsers.js, use <TableUsers ref={ref} />

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import { Button, Container } from "react-bootstrap";
 import "./App.scss";
 import Header from "./components/Header";
 import TableUsers from "./components/TableUsers";
-import ModalAddNew from "./components/ModalAddNew";
-import { useState } from "react";
+import { useRef} from "react";
+import { ToastContainer } from "react-toastify";
 
 function App() {
-  const [isShowModalAddNew, setisShowModalAddNew] = useState(false)
+  const ref = useRef()
+
   return (
     <div className="App">
       <Header />
@@ -14,11 +15,25 @@ function App() {
       <Container>
         <div className="my-3 d-flex justify-content-between align-items-center">
           <span>List Users: </span>
-          <Button variant="success" onClick={()=>setisShowModalAddNew(true)}>Add New User</Button>
+          <Button variant="success" onClick={() => ref.current.setisShowModalAddNew(true)}>
+            Add New User
+          </Button>
         </div>
-        <TableUsers />
+        <TableUsers ref={ref} />
       </Container>
-      <ModalAddNew show={isShowModalAddNew} handleClose={()=>setisShowModalAddNew(false)} />
+      
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
     </div>
     // bọc Container ben ngoai để TableUsers không bị dính sát vào 2 bên lề
   );
