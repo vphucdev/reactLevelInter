00ae03cda7455818db20d57ed2527b9a9e632d40
“phucdev” <vanphuc0295@gmail.com>
2023-10-24 15:03:50 +0700

    create action for modal, handlEdit

--- src/components/ModalAddNew.js
+++ src/components/ModalAddNew.js
@@ -1,45 +1,90 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Button, Modal } from "react-bootstrap";
-import { postCreateUser } from "../services/userServive";
+import { postCreateUser, putUpdateUser } from "../services/userServive";
 import { toast } from "react-toastify";
 
-const ModalAddNew = ({ show, handleClose, handlUpdateTable }) => {
+const ModalAddNew = ({
+  show,
+  handleClose,
+  handlUpdateTable,
+  action,
+  userEdit,
+  handlUpdateFromModal,
+}) => {
   const [name, setName] = useState("");
   const [job, setJob] = useState("");
+
+  const inputRef = useRef();
 
   const handleSaveUser = async () => {
     try {
       const res = await postCreateUser(name, job);
-
-      console.log(res);
       handleClose();
+
       toast.success("Add user successed");
       handlUpdateTable({ id: res.id, first_name: name });
+      setName("");
+      setJob("");
     } catch (error) {
       toast.error("Add user failed");
 
       console.log(error);
     }
   };
+  const handlEdit = async () => {
+    try {
+      handleClose();
+      const res = await putUpdateUser(name, job);
+
+      if (res && res.createdAt) {
+        toast.success("update successed");
+        handlUpdateFromModal({ id: userEdit.id, first_name: name });
+        setName("");
+        setJob("");
+      }
+    } catch (error) {
+      toast.error("Add user failed");
+
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Add new user</Modal.Title>
+          {action === "add" ? (
+            <Modal.Title>Add new user</Modal.Title>
+          ) : (
+            <Modal.Title>Edit user</Modal.Title>
+          )}
         </Modal.Header>
+
         <Modal.Body>
           <div className="body-add-new">
             <div className="mb-3">
               <label htmlFor="exampleInputEmail1" className="form-label">
                 Name
               </label>
-              <input
-                type="text"
-                className="form-control"
-                id="exampleInputEmail1"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
+              {action === "add" ? (
+                <input
+                  ref={inputRef}
+                  type="text"
+                  className="form-control"
+                  id="exampleInputEmail1"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              ) : (
+                <input
+                  ref={inputRef}
+                  type="text"
+                  className="form-control"
+                  id="exampleInputEmail1"
+                  defaultValue={userEdit.first_name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              )}
             </div>
             <div className="mb-3">
               <label htmlFor="exampleInputPassword1" className="form-label">
@@ -59,9 +104,15 @@ const ModalAddNew = ({ show, handleClose, handlUpdateTable }) => {
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleSaveUser}>
-            Save Changes
-          </Button>
+          {action === "add" ? (
+            <Button variant="primary" onClick={handleSaveUser}>
+              Save Changes
+            </Button>
+          ) : (
+            <Button variant="primary" onClick={handlEdit}>
+              Confirm
+            </Button>
+          )}
         </Modal.Footer>
       </Modal>
     </div>
