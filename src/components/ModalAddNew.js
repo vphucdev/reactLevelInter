1ce77bfb4b245e789d8d02cb332c491f0b40e914
“phucdev” <vanphuc0295@gmail.com>
2023-10-26 10:45:31 +0700

    use switch case to render title. create handlConfirmDelete. conditional rendering with prop action ="del"

--- src/components/ModalAddNew.js
+++ src/components/ModalAddNew.js
@@ -1,6 +1,6 @@
 import { useRef, useState } from "react";
 import { Button, Modal } from "react-bootstrap";
-import { postCreateUser, putUpdateUser } from "../services/userServive";
+import { deleteUser, postCreateUser, putUpdateUser } from "../services/userServive";
 import { toast } from "react-toastify";
 import ReactLoading from "react-loading";
 
@@ -11,13 +11,25 @@ const ModalAddNew = ({
   action,
   userEdit,
   handlUpdateFromModal,
+  handlUpdateWhenDelete,
 }) => {
   const [name, setName] = useState("");
   const [job, setJob] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   const inputRef = useRef();
+  
+  const renderTitleModal = () => {
+    switch (action) {
+      case "add":
+        return <Modal.Title>Add new user</Modal.Title>;
+      case "edit":
+        return <Modal.Title>Edit user</Modal.Title>;
 
+      default:
+        return <Modal.Title>Delete user</Modal.Title>;
+    }
+  };
   const handleSaveUser = async () => {
     try {
       setIsLoading(true);
@@ -42,14 +54,14 @@ const ModalAddNew = ({
       const res = await putUpdateUser(name, job);
       setIsLoading(false);
 
-      handleClose();
-
+      
       if (res && res.createdAt) {
         toast.success("update successed");
         handlUpdateFromModal({ id: userEdit.id, first_name: name });
         setName("");
         setJob("");
       }
+      handleClose();
     } catch (error) {
       toast.error("Add user failed");
 
@@ -57,24 +69,39 @@ const ModalAddNew = ({
     }
   };
 
+  const handlConfirmDelete = async () => {
+    setIsLoading(true)
+    let res = await deleteUser(userEdit.id)
+    console.log(res)
+    setIsLoading(false)
+    if (res && res.status === 204) {
+      
+  
+      console.log(res)
+      handlUpdateWhenDelete(userEdit);
+      toast.success("Delete success")
+    } else toast.error("Delete error")
+    handleClose();
+  };
   return (
     <div>
       <Modal show={show} backdrop="static" onHide={handleClose}>
-        <Modal.Header closeButton>
-          {action === "add" ? (
-            <Modal.Title>Add new user</Modal.Title>
-          ) : (
-            <Modal.Title>Edit user</Modal.Title>
-          )}
-        </Modal.Header>
+        <Modal.Header closeButton>{renderTitleModal()}</Modal.Header>
 
         <Modal.Body>
-          <div className="body-add-new">
-            <div className="mb-3">
-              <label htmlFor="exampleInputEmail1" className="form-label">
-                Name
-              </label>
-              {action === "add" ? (
+          {action === "del" ? (
+            <div>
+              <p>Delete this user: </p>
+              <img src={userEdit.avatar} alt="" className="avatar" />
+              <span className="ms-3">{userEdit.first_name}</span>{" "}
+            </div>
+          ) : action === "add" ? (
+            <div className="body-add-new">
+              <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">
+                  Name
+                </label>
+
                 <input
                   ref={inputRef}
                   type="text"
@@ -83,7 +110,27 @@ const ModalAddNew = ({
                   value={name}
                   onChange={(e) => setName(e.target.value)}
                 />
-              ) : (
+              </div>
+              <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">
+                  Job
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  value={job}
+                  onChange={(e) => setJob(e.target.value)}
+                />
+              </div>
+            </div>
+          ) : (
+            <div className="body-add-new">
+              <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">
+                  Name
+                </label>
+
                 <input
                   ref={inputRef}
                   type="text"
@@ -92,21 +139,21 @@ const ModalAddNew = ({
                   defaultValue={userEdit.first_name}
                   onChange={(e) => setName(e.target.value)}
                 />
-              )}
+              </div>
+              <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">
+                  Job
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  value={job}
+                  onChange={(e) => setJob(e.target.value)}
+                />
+              </div>
             </div>
-            <div className="mb-3">
-              <label htmlFor="exampleInputPassword1" className="form-label">
-                Job
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="exampleInputPassword1"
-                value={job}
-                onChange={(e) => setJob(e.target.value)}
-              />
-            </div>
-          </div>
+          )}
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
@@ -126,10 +173,24 @@ const ModalAddNew = ({
                 )}
               </span>
             </Button>
-          ) : (
+          ) : action === "edit" ? (
             <Button variant="primary" onClick={handlEdit}>
               <span className="d-flex gap-2">
                 Confirm
+                {isLoading && (
+                  <ReactLoading
+                    type="spin"
+                    color="#fff"
+                    height={20}
+                    width={20}
+                  />
+                )}
+              </span>
+            </Button>
+          ) : (
+            <Button variant="primary" onClick={handlConfirmDelete}>
+              <span className="d-flex gap-2">
+                Delete user
                 {isLoading && (
                   <ReactLoading
                     type="spin"
