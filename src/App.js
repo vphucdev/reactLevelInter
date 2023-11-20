428498a96fb03ebbdb71cd667efc25ffae850c79
“phucdev” <vanphuc0295@gmail.com>
2023-11-20 16:40:12 +0700

    move List Users in cpn TableUsers

--- src/App.js
+++ src/App.js
@@ -2,26 +2,23 @@ import { Button, Container } from "react-bootstrap";
 import "./App.scss";
 import Header from "./components/Header";
 import TableUsers from "./components/TableUsers";
-import { useRef} from "react";
+import { useRef } from "react";
 import { ToastContainer } from "react-toastify";
 
-function App() {
-  const ref = useRef()
 
+function App() {
+  const ref = useRef();
+  
+  
   return (
     <div className="App">
       <Header />
 
       <Container>
-        <div className="my-3 d-flex justify-content-between align-items-center">
-          <span>List Users: </span>
-          <Button variant="success" onClick={() => ref.current.handlClickAddUser()}>
-            Add New User
-          </Button>
-        </div>
+        
         <TableUsers ref={ref} />
       </Container>
-      
+
       <ToastContainer
         position="top-right"
         autoClose={2000}
@@ -33,9 +30,9 @@ function App() {
         draggable
         pauseOnHover
         theme="colored"
-      />
+        />
     </div>
-    // bọc Container ben ngoai để TableUsers không bị dính sát vào 2 bên lề
+        // bọc Container ben ngoai để TableUsers không bị dính sát vào 2 bên lề
   );
 }
 
