bd7022a5bff9fd7c19633bf68bfd07cab41f78a3
“phucdev” <vanphuc0295@gmail.com>
2023-12-25 15:38:35 +0700

    use router dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import { Button, Container } from "react-bootstrap";
 import "./App.scss";
-import Header from "./components/Header";
-import TableUsers from "./components/TableUsers";
 import { useRef } from "react";
 import { ToastContainer } from "react-toastify";
-
+import { Route, Routes } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout/MainLayout";
+import Home from "./pages/Home";
+import Users from "./pages/Users";
 
 function App() {
-  const ref = useRef();
-  
-  
+  // const ref = useRef();
+
   return (
     <div className="App">
-      <Header />
-
-      <Container>
-        
-        <TableUsers ref={ref} />
-      </Container>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <MainLayout>
+              <Home />
+            </MainLayout>
+          }
+        />
+        <Route
+          path="/users"
+          element={
+            <MainLayout>
+                <Users />
+            </MainLayout>
+          }
+        />
+      </Routes>
 
       <ToastContainer
         position="top-right"
@@ -30,9 +41,9 @@ function App() {
         draggable
         pauseOnHover
         theme="colored"
-        />
+      />
     </div>
-        // bọc Container ben ngoai để TableUsers không bị dính sát vào 2 bên lề
+    // bọc Container ben ngoai để TableUsers không bị dính sát vào 2 bên lề
   );
 }
 
